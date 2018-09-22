d4a7fa52abe7096f1b8e234cef2c01f7e360929a
anuradha151 <anuranasinghe151@gmail.com>
2018-09-22 13:48:29 +0530

    test commit

--- Front-end/src/app/app.module.ts
+++ Front-end/src/app/app.module.ts
@@ -1,17 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './view/dashboard/dashboard.component';
 import { CarMainComponent } from './view/car-main/car-main.component';
+import { NavBarComponent } from './view/nav-bar/nav-bar.component';
+import { FooterComponent } from './view/footer/footer.component';
+import { CustomerMainComponent } from './view/customer-main/customer-main.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'customer_main', component: CustomerMainComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    CarMainComponent
+    CarMainComponent,
+    NavBarComponent,
+    FooterComponent,
+    CustomerMainComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+    ),
     BrowserModule
   ],
   providers: [],
---
+++ Front-end/src/app/view/customer-main/customer-main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-main',
+  templateUrl: './customer-main.component.html',
+  styleUrls: ['./customer-main.component.css']
+})
+export class CustomerMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
