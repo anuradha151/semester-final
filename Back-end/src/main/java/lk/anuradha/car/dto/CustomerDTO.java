91f47867906bfb3148c11449360b721824d1db23
anuradha151 <anuranasinghe151@gmail.com>
2018-09-26 11:10:37 +0530

    customer dto creation finished

--- Back-end/src/main/java/lk/anuradha/car/dto/CustomerDTO.java
+++ Back-end/src/main/java/lk/anuradha/car/dto/CustomerDTO.java
@@ -5,8 +5,79 @@ public class CustomerDTO {
     private String id;
     private String name;
     private String address;
-    String email;
-    String passport;
-    String NIC;
+    private String email;
+    private String passport;
+    private String NIC;
 
+    public CustomerDTO() {
+    }
+
+    public CustomerDTO(String id, String name, String address, String email, String passport, String NIC) {
+        this.setId(id);
+        this.setName(name);
+        this.setAddress(address);
+        this.setEmail(email);
+        this.setPassport(passport);
+        this.setNIC(NIC);
+    }
+
+    @Override
+    public String toString() {
+        return "CustomerDTO{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", email='" + email + '\'' +
+                ", passport='" + passport + '\'' +
+                ", NIC='" + NIC + '\'' +
+                '}';
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassport() {
+        return passport;
+    }
+
+    public void setPassport(String passport) {
+        this.passport = passport;
+    }
+
+    public String getNIC() {
+        return NIC;
+    }
+
+    public void setNIC(String NIC) {
+        this.NIC = NIC;
+    }
 }
