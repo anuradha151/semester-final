b6faa076e3dd1e9211e826aa40439d2036445502
anuradha151 <anuranasinghe151@gmail.com>
2018-09-27 10:56:41 +0530

    Customer service initialized

--- Back-end/src/main/java/lk/anuradha/car/entity/Customer.java
+++ Back-end/src/main/java/lk/anuradha/car/entity/Customer.java
@@ -1,14 +1,11 @@
 package lk.anuradha.car.entity;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Customer {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
     private String name;
     private String address;
--- Back-end/src/main/java/lk/anuradha/car/service/CustomerService.java
+++ Back-end/src/main/java/lk/anuradha/car/service/CustomerService.java
@@ -1,9 +1,18 @@
 package lk.anuradha.car.service;
 
+import lk.anuradha.car.dto.CustomerDTO;
 import org.springframework.http.ResponseEntity;
 
-public class CustomerService {
+public interface CustomerService {
 
+    ResponseEntity<?> saveCustomer(CustomerDTO customerDTO);
 
+    ResponseEntity<?> updateCustomer(CustomerDTO customerDTO);
+
+    ResponseEntity<?> deleteCustomer(CustomerDTO customerDTO);
+
+    ResponseEntity<?> findCustomer(String id);
+
+    ResponseEntity<?> findAllCustomers();
 
 }
