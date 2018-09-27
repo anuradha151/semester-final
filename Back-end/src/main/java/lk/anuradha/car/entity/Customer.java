1da25d788846e65b1e70e09723f01e1534eeade5
anuradha151 <anuranasinghe151@gmail.com>
2018-09-27 10:51:20 +0530

    Customer Entity created

---
+++ Back-end/src/main/java/lk/anuradha/car/entity/Customer.java
@@ -0,0 +1,90 @@
+package lk.anuradha.car.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    private String name;
+    private String address;
+    private String email;
+    private String passport;
+    private String NIC;
+
+    public Customer() {
+    }
+
+    public Customer(String id, String name, String address, String email, String passport, String NIC) {
+        this.setId(id);
+        this.setName(name);
+        this.setAddress(address);
+        this.setEmail(email);
+        this.setPassport(passport);
+        this.setNIC(NIC);
+    }
+
+    @Override
+    public String toString() {
+        return "CustomerDTO{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", email='" + email + '\'' +
+                ", passport='" + passport + '\'' +
+                ", NIC='" + NIC + '\'' +
+                '}';
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassport() {
+        return passport;
+    }
+
+    public void setPassport(String passport) {
+        this.passport = passport;
+    }
+
+    public String getNIC() {
+        return NIC;
+    }
+
+    public void setNIC(String NIC) {
+        this.NIC = NIC;
+    }
+}
--- Back-end/src/main/java/lk/anuradha/car/service/CustomerService.java
+++ Back-end/src/main/java/lk/anuradha/car/service/CustomerService.java
@@ -4,4 +4,6 @@ import org.springframework.http.ResponseEntity;
 
 public class CustomerService {
 
+
+
 }
