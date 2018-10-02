5bcf8a662644d572464e5bc8426a077218ccb794
anuradha151 <anuranasinghe151@gmail.com>
2018-10-02 23:29:37 +0530

    item service implemented

--- Back-end/src/main/java/lk/anuradha/car/service/ItemService.java
+++ Back-end/src/main/java/lk/anuradha/car/service/ItemService.java
@@ -1,4 +1,18 @@
 package lk.anuradha.car.service;
 
-public class ItemService {
+import lk.anuradha.car.dto.ItemDTO;
+import org.springframework.http.ResponseEntity;
+
+public interface ItemService {
+
+    ResponseEntity<?> save(ItemDTO itemDTO) throws Exception;
+
+    ResponseEntity<?> update(ItemDTO itemDTO) throws Exception;
+
+    ResponseEntity<?> delete(ItemDTO itemDTO) throws Exception;
+
+    ResponseEntity<?> findById(long code) throws Exception;
+
+    ResponseEntity<?> getAll() throws Exception;
+
 }
