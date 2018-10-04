ae7345b4eea32679041e6b8cf004bf8ecaa82b3d
anuradha151 <anuranasinghe151@gmail.com>
2018-10-04 23:54:32 +0530

    save method added to item controller

--- Back-end/src/main/java/lk/anuradha/car/controller/ItemController.java
+++ Back-end/src/main/java/lk/anuradha/car/controller/ItemController.java
@@ -1,9 +1,28 @@
 package lk.anuradha.car.controller;
 
+import lk.anuradha.car.dto.ItemDTO;
+import lk.anuradha.car.exception.CustomException;
+import lk.anuradha.car.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/item")
 public class ItemController {
+    @Autowired
+    private ItemService itemService;
+
+    @PostMapping
+    public ResponseEntity<?> saveCustomer(@RequestBody ItemDTO itemDTO) {
+        try {
+            return itemService.save(itemDTO);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new CustomException("Failed to Save item. Operation unsuccessful");
+        }
+    }
 }
