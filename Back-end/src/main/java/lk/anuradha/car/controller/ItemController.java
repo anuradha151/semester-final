8a6c7feb1676d4a454eb8320fbef2daafbcbaebd
anuradha151 <anuranasinghe151@gmail.com>
2018-10-04 23:50:32 +0530

    Item controller initialized

---
+++ Back-end/src/main/java/lk/anuradha/car/controller/ItemController.java
@@ -0,0 +1,8 @@
+package lk.anuradha.car.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+public class ItemController {
+}
