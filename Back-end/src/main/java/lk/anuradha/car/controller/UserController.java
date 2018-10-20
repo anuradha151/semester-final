e15699b9fa42bbc1098bd04d9733994337ce486b
anuradha151 <anuranasinghe151@gmail.com>
2018-10-20 08:15:07 +0530

    user controller created

---
+++ Back-end/src/main/java/lk/anuradha/car/controller/UserController.java
@@ -0,0 +1,20 @@
+package lk.anuradha.car.controller;
+
+import lk.anuradha.car.dto.UserDTO;
+import lk.anuradha.car.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+
+    @PostMapping("login")
+    public boolean canAuthenticate(@RequestBody UserDTO user) {
+        return userService.canAuthenticate(user.getUsername(), user.getPassword());
+    }
+}
