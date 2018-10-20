a7dad695b58c4ce88e2ada8c315a64ea741e1785
anuradha151 <anuranasinghe151@gmail.com>
2018-10-20 08:09:40 +0530

    user service impl added

---
+++ Back-end/src/main/java/lk/anuradha/car/service/impl/UserServiceImpl.java
@@ -0,0 +1,31 @@
+package lk.anuradha.car.service.impl;
+
+import lk.anuradha.car.model.User;
+import lk.anuradha.car.repository.UserRepository;
+import lk.anuradha.car.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public boolean canAuthenticate(String username, String password) {
+        boolean exists = userRepository.existsById(username);
+
+        if (!exists) {
+            return false;
+        }
+
+        User user = userRepository.findById(username).get();
+
+        return user.getPassword().equals(password);
+    }
+}
