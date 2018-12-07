76828d08bade7b534ae8dc5f313107f7505cd3a3
anuradha151 <anuranasinghe151@gmail.com>
2018-12-07 08:11:03 +0530

    user service and controllers updated

--- Back-end/src/main/java/lk/anuradha/car/controller/UserController.java
+++ Back-end/src/main/java/lk/anuradha/car/controller/UserController.java
@@ -3,14 +3,22 @@ package lk.anuradha.car.controller;
 import lk.anuradha.car.dto.UserDTO;
 import lk.anuradha.car.service.custom.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/user")
 public class UserController {
+
+    private final UserService userService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @Autowired
-    private UserService userService;
+    public UserController(UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userService = userService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
 
 
     @PostMapping("login")
