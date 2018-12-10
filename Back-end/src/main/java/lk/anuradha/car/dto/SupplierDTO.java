311d2db89d3fa142249115f1c6d7ecea0ebcaa0f
anuradha151 <anuranasinghe151@gmail.com>
2018-12-10 11:04:02 +0530

    SupplierDTO created

--- Back-end/src/main/java/lk/anuradha/car/dto/SupplierDTO.java
+++ Back-end/src/main/java/lk/anuradha/car/dto/SupplierDTO.java
@@ -1,10 +1,71 @@
 package lk.anuradha.car.dto;
 
 public class SupplierDTO {
-    long id;
-    String name;
-    String email;
-    String address;
-    String contactNumber;
+    private long id;
+    private String name;
+    private String email;
+    private String address;
+    private String contactNumber;
 
+    public SupplierDTO() {
+    }
+
+    public SupplierDTO(long id, String name, String email, String address, String contactNumber) {
+        this.setId(id);
+        this.setName(name);
+        this.setEmail(email);
+        this.setAddress(address);
+        this.setContactNumber(contactNumber);
+    }
+
+    @Override
+    public String toString() {
+        return "SupplierDTO{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                ", contactNumber='" + contactNumber + '\'' +
+                '}';
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
 }
