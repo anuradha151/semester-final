410bcfc5be4e9fdbc2bcac3c7e1514101b3aecd7
anuradha151 <anuranasinghe151@gmail.com>
2018-12-10 11:06:01 +0530

    Supplier model created

--- Back-end/src/main/java/lk/anuradha/car/model/Supplier.java
+++ Back-end/src/main/java/lk/anuradha/car/model/Supplier.java
@@ -1,10 +1,77 @@
 package lk.anuradha.car.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Supplier {
-    long id;
-    String name;
-    String email;
-    String address;
-    String contactNumber;
+    @Id
+    private long id;
+    private String name;
+    private String email;
+    private String address;
+    private String contactNumber;
+
+    public Supplier() {
+    }
+
+    public Supplier(long id, String name, String email, String address, String contactNumber) {
+        this.setId(id);
+        this.setName(name);
+        this.setEmail(email);
+        this.setAddress(address);
+        this.setContactNumber(contactNumber);
+    }
+
+    @Override
+    public String toString() {
+        return "SupplierDTO{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                ", contactNumber='" + contactNumber + '\'' +
+                '}';
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
 
 }
