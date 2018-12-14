5a63d0301049549036f456d20b1c8b76abeab891
anuradha151 <anuranasinghe151@gmail.com>
2018-12-14 15:53:43 +0530

    item service crud service implemented

--- Back-end/src/main/java/lk/anuradha/car/service/custom/ItemService.java
+++ Back-end/src/main/java/lk/anuradha/car/service/custom/ItemService.java
@@ -1,10 +1,9 @@
 package lk.anuradha.car.service.custom;
 
 import lk.anuradha.car.dto.ItemDTO;
-import lk.anuradha.car.model.Item;
 import lk.anuradha.car.service.CrudService;
 
-public interface ItemService extends CrudService<ItemDTO, Item> {
+public interface ItemService extends CrudService<ItemDTO> {
 
 
 }
